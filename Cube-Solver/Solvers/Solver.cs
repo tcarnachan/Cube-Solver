bcae12f76211ff61913c9f15adb56ef33b2a6c7a
tcarnachan <thomas.carnachan@gmail.com>
2020-07-21 19:44:21 +0100

    Code for generating pruning tables for phase 2
    onesLookup, eslice2ix, and factorials are used to cache values for calculating ids

--- Cube-Solver/Solvers/Solver.cs
+++ Cube-Solver/Solvers/Solver.cs
@@ -19,17 +19,28 @@ namespace Cube_Solver.Solvers
         private const string epPath = "res/edge_perm";
 
         private PruningTable coTable, eoTable, cpTable, epTable;
+        private int[] onesLookup = new int[NUM_CPS];
         private Dictionary<int, int> eslice2ix = new Dictionary<int, int>();
+        private int[] factorials;
 
         public Solver(CubieCube solved)
         {
-            // Lookup table for calculating ids
+            // Lookup tables for calculating ids
+            for (int i = 0; i < NUM_CPS; i++)
+                onesLookup[i] = BitCount(i);
             for (int i = 0, j = 0; i < NUM_ESLICE; i++, j++)
             {
-                int bits = BitCount(j);
+                int bits = onesLookup[j];
                 while (bits != 3 && bits != 4)
-                    bits = BitCount(++j);
+                    bits = onesLookup[++j];
                 eslice2ix[j] = i;
+            }
+            factorials = new int[solved.ep.Length];
+            factorials[0] = 1;
+            for (int fact = 1, i = 1; i < solved.ep.Length; ++i)
+            {
+                fact *= i;
+                factorials[i] = fact;
             }
 
             // All moves are valid for phase 1
@@ -40,6 +51,7 @@ namespace Cube_Solver.Solvers
                     phase1moves.Add((f, dir));
             }
 
+            // Initialise phase 1 pruning tables
             if (File.Exists(coPath))
                 coTable = new PruningTable(coPath);
             else
@@ -55,6 +67,38 @@ namespace Cube_Solver.Solvers
             {
                 Console.WriteLine("Generating edge orientation tables...");
                 eoTable = new PruningTable(solved, NUM_EOS, cc => GetEO(cc) * NUM_ESLICE + GetEslice(cc), phase1moves, eoPath);
+                Console.WriteLine("done\n");
+            }
+
+            // Phase 2 <U, D, F2, B2, R2, L2>
+            List<(Cube.Face, Cube.Dir)> phase2moves = new List<(Cube.Face, Cube.Dir)>();
+            foreach (Cube.Face f in Enum.GetValues(typeof(Cube.Face)))
+            {
+                if (f == Cube.Face.U || f == Cube.Face.D)
+                {
+                    foreach (Cube.Dir dir in Enum.GetValues(typeof(Cube.Dir)))
+                        phase2moves.Add((f, dir));
+                }
+                else
+                    phase2moves.Add((f, Cube.Dir.HALF));
+            }
+
+            // Initialise phase 2 pruning tables
+            if (File.Exists(cpPath))
+                cpTable = new PruningTable(cpPath);
+            else
+            {
+                Console.WriteLine("Generating corner permutation tables...");
+                cpTable = new PruningTable(solved, NUM_CPS, GetCP, phase2moves, cpPath);
+                Console.WriteLine("done\n");
+            }
+
+            if (File.Exists(epPath))
+                eoTable = new PruningTable(epPath);
+            else
+            {
+                Console.WriteLine("Generating edge permutation tables...");
+                epTable = new PruningTable(solved, NUM_EPS, GetEP, phase2moves, epPath);
                 Console.WriteLine("done\n");
             }
         }
@@ -103,14 +147,37 @@ namespace Cube_Solver.Solvers
             return eslice2ix[esp];
         }
 
-        private static int GetCP(CubieCube cc)
+        private int GetCP(CubieCube cc)
         {
-            return -1;
+            int id = 0, seen = 0;
+            for (int i = 0; i < cc.cp.Length - 1; ++i)
+            {
+                seen |= 1 << (cc.cp.Length - 1 - cc.cp[i]);
+                int less = onesLookup[seen >> (cc.cp.Length - cc.cp[i])];
+                id += (cc.cp[i] - less) * factorials[cc.cp.Length - 1 - i];
+            }
+            return id;
         }
 
-        private static int GetEP(CubieCube cc)
+        private int GetEP(CubieCube cc)
         {
-            return -1;
+            int id = 0, seen = 0;
+            for (int i = 0; i < cc.cp.Length - 1; ++i)
+            {
+                seen |= 1 << (cc.cp.Length - 1 - cc.ep[i]);
+                int less = onesLookup[seen >> (cc.cp.Length - cc.ep[i])];
+                id += (cc.ep[i] - less) * factorials[cc.cp.Length - 1 - i];
+            }
+            id *= 24;
+            seen = 0;
+            int BL = (int)Cube.Edge.BL;
+            for (int i = 0; i < 3; ++i)
+            {
+                seen |= 1 << (3 - cc.ep[i + BL] + BL);
+                int less = onesLookup[seen >> (4 - cc.ep[i + BL] + BL)];
+                id += (cc.ep[i + BL] - BL - less) * factorials[3 - i];
+            }
+            return id;
         }
 
         public int[] Solve() { throw new NotImplementedException(); }
