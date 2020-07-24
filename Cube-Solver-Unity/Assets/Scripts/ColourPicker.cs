9d4b228f786dc58b6728542ead94c84becd7c2ee
tcarnachan <thomas.carnachan@gmail.com>
2020-07-24 18:18:10 +0100

    Add ability to select and place colours
    There are 27 buttons representing a map view of the cube. There are 6 different coloured buttons which allows the user to select a colour and apply it to the map buttons.

---
+++ Cube-Solver-Unity/Assets/Scripts/ColourPicker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourPicker : MonoBehaviour
+{
+    // Currently selected colour
+    private Color selected;
+    // Cube face map
+    public Transform map;
+    // Colour buttons
+    public Image[] colours;
+
+    private void Start()
+    {
+        // Add listeners to all facelets on the cube map
+        foreach(Transform face in map)
+        {
+            foreach(Transform facelet in face)
+                facelet.gameObject.AddComponent<Button>().onClick.AddListener(() => PlaceColour(facelet));
+        }
+        // Add listeners to all colour buttons
+        foreach (Image img in colours)
+            img.gameObject.AddComponent<Button>().onClick.AddListener(() => SelectColour(img));
+        // Initialise selected colour
+        selected = colours[0].color;
+    }
+
+    private void SelectColour(Image img)
+    {
+        selected = img.color;
+    }
+
+    private void PlaceColour(Transform img)
+    {
+        img.GetComponent<Image>().color = selected;
+    }
+}
